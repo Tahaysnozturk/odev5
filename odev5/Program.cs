a5eb90ad81cd08874025f5433ee315ef661daccf
tiyol <tiyol@LAPTOP-6IPT3PBJ>
2024-01-07 00:14:29 +0300

    Proje dosyası ekle.

---
+++ odev5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Bir sayı girin: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("TEK'leri mi ÇİFT'leri mi toplamak istersiniz? (TEK/ÇİFT): ");
+        string tercih = Console.ReadLine().ToUpper();
+
+        int toplam = 0;
+
+        if (tercih == "TEK")
+        {
+            for (int i = 1; i <= sayi; i += 2)
+            {
+                toplam += i;
+            }
+        }
+        else if (tercih == "ÇİFT")
+        {
+            for (int i = 2; i <= sayi; i += 2)
+            {
+                toplam += i;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Geçersiz seçim!");
+            return;
+        }
+
+        Console.WriteLine("Toplam = " + toplam);
+    }
+}
